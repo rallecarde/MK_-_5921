4ce37a7326e75e56d5c2a2166f5b050d7ffab135
rallecarde <39776771+rallecarde@users.noreply.github.com>
2021-06-01 02:51:30 +0300

    h файл библиотеки для dht11

--- dht11.h
+++ dht11.h
@@ -9,16 +9,17 @@
 
 #define DHT11LIB_VERSION "0.4.1"
 
-#define DHTLIB_OK        0
-#define DHTLIB_ERROR_CHECKSUM -1
-#define DHTLIB_ERROR_TIMEOUT  -2
+#define DHTLIB_OK				0
+#define DHTLIB_ERROR_CHECKSUM	-1
+#define DHTLIB_ERROR_TIMEOUT	-2
 
 class dht11
 {
 public:
     int read(int pin);
-  int humidity;
-  int temperature;
+	int humidity;
+	int temperature;
 };
 #endif
 // END OF FILE
+
